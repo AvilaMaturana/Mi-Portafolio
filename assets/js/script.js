c0ea48015ef5be994fa67c7ba40a8a14f468dacd
Diego Ávila Maturana <94457305+AvilaMaturana@users.noreply.github.com>
2022-01-13 16:05:09 -0300

    Se arreglo navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,14 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
-window.addEventListener('load', function(){
-	new Glider(document.querySelector('.carousel__lista'), {
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: '.carousel__indicadores',
-		arrows: {
-			prev: '.carousel__anterior',
-			next: '.carousel__siguiente'
-		},
-		responsive: [
-			{
-			  // screens greater than >= 775px
-			  breakpoint: 450,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 1,
-				slidesToScroll: 1
-			  }
-			},{
-			  // screens greater than >= 1024px
-			  breakpoint: 800,
-			  settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			  }
-			}
-		]
-	});
-});
+$(document).ready(function(){
 
+	window.addEventListener('scroll', function(){
+		var nav = document.querySelector('nav');
+		nav.classList.toggle('abajo' , window.scrollY>0);
+	});
+
+});
 
 
 var myModal = document.getElementById('myModal')
@@ -43,15 +21,4 @@ myModal.addEventListener('shown.bs.modal', function () {
 });
 
 
-// $(window).scroll(function(){
-// 	if($('#menu').offset().top > 56){
-// 		$('#menu').addClass('nuevoNav');
-// 	} else{
-// 		$('#menu').removeClass('nuevoNav');
-// 	};
-// });
 
-// window.addEventListener('scroll', function(){
-//     var nav = document.querySelector('nav');
-//     nav.classList.toggle('abajo' , window.scrollY>0);
-// });
